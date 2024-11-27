da74f56ed46f9ee5c65103bbb1b0814e999b6841
carloskes <cscorra@utpl.edu.ec>
2024-11-27 11:57:14 -0500

    actualizado

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
@@ -7,13 +7,18 @@
  sean ingresados por teclado por el usuario.
  */
 package ejemplos02;
+import java.util.Locale;
+import java.util.Scanner;
 /**
  *
  * @author reroes
  */
 public class Ejemplo05 {
+    
+        
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         // 2. numero, i[0, n]
         int numero;
         // 3. potencia, i[0, n]
@@ -21,13 +26,19 @@ public class Ejemplo05 {
         // 6. resultado, i[0, n]
         int resultado;
         // numero <-- 4
-        numero = 4;
+        
         // 8. potencia <-- 3
-        potencia = 3;
+        
         // 9. resultado <-- 1
         resultado = 1;
 
         int contador = 1;
+        
+        System.out.println("Ingrese el número a potenciar");
+            numero = entrada.nextInt();
+            
+            System.out.println("Ingrese la potencia");
+            potencia = entrada.nextInt();
 
         do {
             resultado = resultado * numero;
